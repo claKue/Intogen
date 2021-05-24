47fc10c84b2e256670a7266334871f6de4290807
claKue <clarissa.kuemhof@stud.uni-due.de>
2021-05-24 10:27:19 +0200

    Treffen Änderungen

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,7 +10,7 @@ function HeroSection() {
             <p>Intogen is David Kolbs's research based web service which provides Uni-Due ISE students to identify thire preferred learning styles. 
             Learning style's data will be used as in to provide overview on the basis of there nationalities and education, also it will allow participants to do the survey online and see there results along with suggestions about courses which they might like to study with liked type learners.
            </p>
-           <img src={img1} alt="learning styles" />
+           <img className="bildHome" src={img1} alt="learning styles" />
         </div>
     )
 }
--- src/components/Pages/AboutUs.js
+++ src/components/Pages/AboutUs.js
@@ -8,13 +8,13 @@ export default function AboutUs() {
         <>
         <h1 className="aboutus">ABOUT US</h1>
         <h2>Welcome to Intogen</h2>
-            <p>We are students from Uni-DUE (Universität Duisburg - Essen) ISE program Computer Engineering department (CE) major (Intelligent Networked Systems) </p>
-            <p>This web application is our practical project (Praxis projekt) under the supervision of Prof. Dr. Mohamed Amine Chatti. </p>
-            <p>To provide this application, we needed to collect data from other ISE students. Thus we used Sample Surveys method to collect data. </p>
-            <p>You can see the survey questions under Find Your Type.</p>
-            <p>What will happen to your data?</p>
-            <p>Each participant is anonymous by nature of the survey, this data will be used for visual analytics to help students to know about their learning styles and interesting visual analytics to check course suitability.</p>
-            <p>Teachers and organisation can also use the service to improve learning experience of students later on when the web service is developed.
+            <p className="aboutUs">We are students from Uni-DUE (Universität Duisburg - Essen) ISE program Computer Engineering department (CE) major (Intelligent Networked Systems) 
+            This web application is our practical project (Praxis projekt) under the supervision of Prof. Dr. Mohamed Amine Chatti.
+            To provide this application, we needed to collect data from other ISE students. Thus we used Sample Surveys method to collect data.
+            You can see the survey questions under Find Your Type. <br/>
+            What will happen to your data? <br/>
+            Each participant is anonymous by nature of the survey, this data will be used for visual analytics to help students to know about their learning styles and interesting visual analytics to check course suitability.
+            Teachers and organisation can also use the service to improve learning experience of students later on when the web service is developed.
             </p>
            <img src={img2} alt="UDE" />
 
