890822a9fcee0eafaa2d7f9ee2809eba287101f8
claKue <clarissa.kuemhof@stud.uni-due.de>
2021-05-24 16:22:26 +0200

    Kleinigkeiten

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -51,7 +51,9 @@ function Navbar() {
                         </li>
                     </ul>
                 </div>
+                
             </nav>
+            <hr class="border2" />
         </>
     )
 }
